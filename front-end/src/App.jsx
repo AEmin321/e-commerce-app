2817327bf2409a0953119eb2c0a9dda8a4728574
hamini <ahmademin321@gmail.com>
2024-02-29 16:17:02 +0300

    Added the signup page

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,11 +1,14 @@
 // import Home from "./pages/Home"
+import SignUp from "./pages/SignUp"
 import Login from "./pages/Login"
+import Home from "./pages/Home"
+
 
 function App() {
 
   return (
     <div>
-      <Login />
+      <SignUp />
     </div>
   )
 }
---
+++ front-end/src/pages/SignUp.jsx
@@ -0,0 +1,74 @@
+import styled from "styled-components"
+import { footer, mdMobile } from "../responsive"
+
+const Container = styled.div`
+    height: 100vh;
+    width: 100vw;
+    display: flex;
+    background-color: #43a4e3;
+    justify-content: center;
+    align-items: center;
+`
+
+const Wrapper = styled.div`
+    background-color: white;
+    width: 30%;
+    padding: 37px;
+    ${footer({width:"70%"})}
+    ${mdMobile({width:"65%"})}
+`
+
+const Title = styled.h1`
+    margin-bottom: 25px;
+    font-weight: 500;
+`
+
+const Form = styled.form`
+    display: flex;
+    justify-content: center;
+    flex-wrap:wrap;
+`
+
+const Input = styled.input`
+    flex: 1;
+    min-width: 40%;
+    margin: 20px 10px 0px 0px;
+    padding: 10px;
+`
+
+const Agreement = styled.p`
+    font-size: 12px;
+    margin: 12px 0;
+`
+
+const Button = styled.button`
+    padding: 10px 22px;
+    width: 100%;
+    border: none;
+    outline: none;
+    background-color: #1d1d1d;
+    color: white;
+    cursor: pointer;
+`
+
+const SignUp = () => {
+  return (
+    <Container>
+        <Wrapper>
+            <Title>Create New Account</Title>
+            <Form>
+                <Input placeholder="Username" />
+                <Input placeholder="Name" />
+                <Input placeholder="Lastname" />
+                <Input type="password" placeholder="Password" />
+                <Input type="password" placeholder="Confirm password" />
+                <Agreement>By creating an account, I consent to the processing of my personal
+            data in accordance with the <a href="#">PRIVACY POLICY</a></Agreement>
+                <Button>Create</Button>
+            </Form>
+        </Wrapper>
+    </Container>
+  )
+}
+
+export default SignUp
