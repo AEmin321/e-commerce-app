160c802e804caf3b7a7b2e2cb700efc30b921687
hamini <ahmademin321@gmail.com>
2024-03-05 20:47:26 +0300

    update

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,16 +1,16 @@
 // import ProductsList from "./pages/ProductsList"
 // import SignUp from "./pages/SignUp"
 // import Login from "./pages/Login"
-// import Home from "./pages/Home"
+import Home from "./pages/Home"
 // import Product from "./pages/Product"
-import Cart from "./pages/Cart"
+// import Cart from "./pages/Cart"
 
 
 function App() {
 
   return (
     <div>
-      <Cart/>
+      <Home/>
     </div>
   )
 }
