bf0e861db239f5a5f6575b42c840e18b1c799e36
hamini <ahmademin321@gmail.com>
2024-04-30 02:15:05 +0300

    Add: login route

--- back-end/controllers/auth.js
+++ back-end/controllers/auth.js
@@ -4,6 +4,27 @@ import CryptoJS from "crypto-js";
 
 const authRouter = Router();
 
+authRouter.post("/login", async (req, res) => {
+  const { username, password } = req.body;
+  try {
+    const user = await User.findOne({ username: username });
+    if (!user) {
+      return res.status(401).json("Wrong Credentials!!!");
+    }
+    const hashed_password = CryptoJS.AES.decrypt(
+      user.password,
+      process.env.SECRET
+    );
+    const pass = hashed_password.toString(CryptoJS.enc.Utf8);
+    if (pass !== password) {
+      return res.status(401).json("Wrong Password!!!");
+    }
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
 authRouter.post("/register", async (req, res) => {
   const { username, email, password } = req.body;
   const user = new User({
