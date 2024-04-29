5188d564a655f1fdbab84c8a87c89546c104ad97
hamini <ahmademin321@gmail.com>
2024-04-29 03:59:55 +0300

    Add: setup and connection to mongodb

--- back-end/index.js
+++ back-end/index.js
@@ -1,1 +1,20 @@
-console.log("helloo");
+import express from "express";
+import mongoose from "mongoose";
+import dontenv from "dotenv";
+
+dontenv.config();
+const app = express();
+
+console.log(process.env.MONGO_URL);
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => {
+    console.log("Connected to database");
+  })
+  .catch((error) => {
+    console.log("Can't connect to database");
+  });
+
+app.listen(process.env.PORT || 3002, () => {
+  console.log("Server is running.");
+});
