c957941fade2eea9f48a14ef26dd9934de37571e
hamini <ahmademin321@gmail.com>
2024-05-02 20:47:12 +0300

    Add: stats route to get last year user total each month

--- back-end/controllers/users.js
+++ back-end/controllers/users.js
@@ -20,6 +20,30 @@ userRouter.get("/", userAdminAuthorization, async (req, res) => {
   }
 });
 
+userRouter.get("/stats", userAdminAuthorization, async (req, res) => {
+  const date = new Date();
+  const lastYear = new Date(date.setFullYear(date.getFullYear() - 1));
+  try {
+    const data = await User.aggregate([
+      {
+        $match: { createdAt: { $gte: lastYear } },
+      },
+      {
+        $project: { month: { $month: "$createdAt" } },
+      },
+      {
+        $group: {
+          _id: "$month",
+          total: { $sum: 1 },
+        },
+      },
+    ]);
+    res.status(200).json(data);
+  } catch (error) {
+    res.status(500).json(err);
+  }
+});
+
 userRouter.get("/:id", userAdminAuthorization, async (req, res) => {
   try {
     const user = await User.findById(req.params.id);
