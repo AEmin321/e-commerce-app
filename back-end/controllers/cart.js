a9139196835789c85dd6802aae2db7e66f02a939
hamini <ahmademin321@gmail.com>
2024-05-03 19:45:57 +0300

    add: deletion route for cart

--- back-end/controllers/cart.js
+++ back-end/controllers/cart.js
@@ -4,6 +4,16 @@ import { verifyUser, userAuthorization } from "../middlewares/verifyToken";
 
 const cartRouter = express.Router();
 
+cartRouter.delete("/:id", userAuthorization, async (req, res) => {
+  const id = req.params.id;
+  try {
+    await Cart.findByIdAndDelete(id);
+    res.status(200).json("item removed successfully.");
+  } catch (error) {
+    res.status("500").json("Can't delete the product.");
+  }
+});
+
 cartRouter.put("/:id", userAuthorization, async (req, res) => {
   try {
     const updatedCart = await Cart.findByIdAndUpdate(
