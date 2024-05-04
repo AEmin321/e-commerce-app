d2e70817263aa346bb1b65dfe01d68c0d00ba394
hamini <ahmademin321@gmail.com>
2024-05-04 18:29:38 +0300

    add: route for getting single user cart and all user carts as admin

--- back-end/controllers/cart.js
+++ back-end/controllers/cart.js
@@ -41,4 +41,22 @@ cartRouter.post("/", verifyUser, async (req, res) => {
   }
 });
 
+cartRouter.get("/", userAuthorization, async (req, res) => {
+  try {
+    const carts = await Cart.find();
+    res.status(200).json(carts);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+cartRouter.get("/:userId", userAuthorization, async (req, res) => {
+  try {
+    const cart = await Cart.findOne({ userId: req.params.userId });
+    res.status(200).json(cart);
+  } catch (error) {
+    res.status(500).json("Can't find the user cart.");
+  }
+});
+
 export default cartRouter;
